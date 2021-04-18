6a1b285b28f1919415deabeca371c65bf37f3af9
lonnen <chris.lonnen@gmail.com>
2021-04-17 20:05:52 -0700

    newline needed on the linter config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    env: {
-      browser: false,
-      es2021: true,
-    },
-    extends: [
-      'airbnb-base',
-    ],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {},
-  };
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+};

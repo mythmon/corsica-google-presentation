f3fc2a3c9849bddc4746e8e6876b717dd44816ae
lonnen <chris.lonnen@gmail.com>
2021-04-19 16:20:14 -0700

    random strategy test
    let's strip it down and do one test at a time

--- tests.js
+++ tests.js
@@ -1,32 +1,17 @@
 const assert = require('assert');
 const { describe, it } = require('mocha');
 
-const { computeSlideIndex, computeSlideUrl, getSlideIds } = require('.');
+const { slideChooser, buildSlideURL, getSlideIds } = require('.');
 const corsicaDemoPresentationDeckID = '1_RWJt6XslTBeB04XjYJk71mM7DR49YKDVmZc5ZlWwUo';
 
 describe('the non-corsica business logic', () => {
   it('should choose a valid random index', () => {
-    let chosenIndex = -1;
+    let values = [10,20,30,40,50];
     // random slides
     for (let i = 0; i < 10; i++) {
-        chosenIndex = computeSlideIndex('random', 10);
-        assert.ok(chosenIndex >= 0);
-        assert.ok(chosenIndex < 10);
-        assert.ok(Number.isInteger(chosenIndex));
+        chosenItem = slideChooser('random')(values);
+        assert.ok(values.indexOf(chosenItem) >= 0);
+        assert.ok(values.indexOf(chosenItem) < 5);
     }
-  });
-  it('should try to choose the numbered slide, but safely choose 0 if things get weird', () => {
-    for (let i = 0; i < 10; i++) {
-      chosenIndex = computeSlideIndex(i, 10);
-      assert.ok(chosenIndex == i);
-    }
-
-    // out of bounds
-    chosenIndex = computeSlideIndex(11, 10);
-    assert.ok(chosenIndex == 0);
-
-    // text other than random
-    chosenIndex = computeSlideIndex('five', 10);
-    assert.ok(chosenIndex == 0);
   });
 });

47dd625d4e4d5352406b75a9d9aab67c1b71fb35
lonnen <chris.lonnen@gmail.com>
2021-04-20 01:09:04 -0700

    requires should be spaced out

--- tests.js
+++ tests.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const { describe, it } = require('mocha');
 
 const { slideChooser, buildSlideURL, getSlideIds } = require('.');
+
 const corsicaDemoPresentationDeckID = '1_RWJt6XslTBeB04XjYJk71mM7DR49YKDVmZc5ZlWwUo';
 
 describe('the non-corsica business logic', () => {

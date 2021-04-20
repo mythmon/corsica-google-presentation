29a75b32ffb349686a64b6806e13013c532de9ec
lonnen <chris.lonnen@gmail.com>
2021-04-20 01:51:48 -0700

    hoise demoslideID to a constant

--- tests.js
+++ tests.js
@@ -4,6 +4,7 @@ const { describe, it } = require('mocha');
 const { slideChooser, buildSlideURL, getSlideIds } = require('.');
 
 const corsicaDemoPresentationDeckID = '1_RWJt6XslTBeB04XjYJk71mM7DR49YKDVmZc5ZlWwUo';
+const corsicaDemoSlideID = 'id.g7e85c7fed4_1_36';
 
 describe('the non-corsica business logic', () => {
   it('should choose a valid random index', () => {
@@ -39,7 +40,7 @@ describe('the non-corsica business logic', () => {
 
   it('should build a valid url', () => {
     assert.strictEqual(
-      buildSlideURL(corsicaDemoPresentationDeckID)('id.g7e85c7fed4_1_36'),
+      buildSlideURL(corsicaDemoPresentationDeckID)(corsicaDemoSlideID),
       'https://docs.google.com/presentation/d/1_RWJt6XslTBeB04XjYJk71mM7DR49YKDVmZc5ZlWwUo/embed#slide=id.id.g7e85c7fed4_1_36',
     );
   });

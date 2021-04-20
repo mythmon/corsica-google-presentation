a920ebb5a19a0dc92aa8fbb2708806168b3db618
lonnen <chris.lonnen@gmail.com>
2021-04-20 01:14:00 -0700

    off by one errors

--- tests.js
+++ tests.js
@@ -7,7 +7,7 @@ const corsicaDemoPresentationDeckID = '1_RWJt6XslTBeB04XjYJk71mM7DR49YKDVmZc5ZlW
 
 describe('the non-corsica business logic', () => {
   it('should choose a valid random index', () => {
-    let values = [10, 20, 30, 40, 50];
+    const values = [10, 20, 30, 40, 50];
     // random slides
     let chosenItem = 0;
     for (let i = 0; i < 10; i += 1) {
@@ -23,7 +23,7 @@ describe('the non-corsica business logic', () => {
     for (let i = 0; i < values.length; i += 1) {
       chosenItem = slideChooser(i)(values);
       assert.strictEqual(values.indexOf(chosenItem), i);
-      assert.strictEqual(chosenItem, i*10);
+      assert.strictEqual(chosenItem, (i + 1) * 10);
     }
 
     // out of bounds is undefined
